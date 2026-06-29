21df3088f7f59a6df0b53611eca589bd53eccdca
nihirchandila <94970433+nihirchandila@users.noreply.github.com>
2026-06-30 00:21:57 +0530

    fixed root directory issue in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,20 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
+
 import connectDB from "./config/db.js";
 import agentRoutes from "./routes/agentRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import propertyRoutes from "./routes/propertyRoutes.js";
 import contactRoutes from "./routes/contactRoutes.js";
-
-//hosting configuration
-const path = require("path");
-const __dirname = path.resolve();
-app.use(
-  "/uploads",
-  express.static(path.join(__dirname, "../uploads"))
-)
-
 
 dotenv.config();
 
@@ -25,19 +17,27 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
 
+const rootDir = path.resolve();
+
+// Serve uploaded files
+app.use(
+  "/uploads",
+  express.static(path.join(rootDir, "../backend/uploads"))
+);
+
+// API Routes
 app.use("/api/property", propertyRoutes);
 app.use("/api/agent", agentRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/contact", contactRoutes);
 
+// Serve React build
+app.use(express.static(path.join(rootDir, "../client/dist")));
 
-app.use(express.static(path.join(__dirname, "../../client/dist")));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+app.get("/{*any}", (req, res) => {
+  res.sendFile(path.join(rootDir, "../client/dist/index.html"));
 });
-
 
 const PORT = process.env.PORT || 3000;
 
